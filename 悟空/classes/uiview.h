f1f188c8cb5a5d960b8ccfa91daae5ddf566867e
songgangqiang0619 <790030601@qq.com>
2016-09-02 17:12:29 +0800

    添加uiview试试
    测试

---
+++ 悟空/classes/uiview.h
@@ -0,0 +1,13 @@
+//
+//  uiview.h
+//  悟空
+//
+//  Created by 宋钢强 on 16/9/2.
+//  Copyright © 2016年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface uiview : UIView
+
+@end
---
+++ 悟空/classes/uiview.m
@@ -0,0 +1,21 @@
+//
+//  uiview.m
+//  悟空
+//
+//  Created by 宋钢强 on 16/9/2.
+//  Copyright © 2016年 alibaba. All rights reserved.
+//
+
+#import "uiview.h"
+
+@implementation uiview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
